5aad43a230e75cc9344115b2e2124881e0f4d3d2
AlexyCatelle <alexy.catelle@oclock.school>
2025-07-08 14:13:48 +0200

    Sous-menu evenement en cours.

--- src/main/java/TPBillet/IHM.java
+++ src/main/java/TPBillet/IHM.java
@@ -29,7 +29,7 @@ public class IHM {
                 switch (choice) {
                     case 1 -> System.out.println("Option 1");
                     case 2 -> System.out.println("Option 2");
-                    case 3 -> System.out.println("Option 3");
+                    case 3 -> manageEvents();
                     case 4 -> System.out.println("Option 4");
                     case 5 -> System.out.println("Option 5");
                     case 6 -> System.out.println("Option 6");
@@ -39,6 +39,33 @@ public class IHM {
                 System.out.println("Erreur :" + e.getMessage());
             }
         }
+    }
+
+    public void manageEvents(){
+        int choice = -1;
+        while (choice != 5) {
+        System.out.println("\n-- GESTION DES EVENEMENTS --");
+        System.out.println("1. Lister les événements.");
+        System.out.println("2. Créer un événement.");
+        System.out.println("3. Modifier un événement.");
+        System.out.println("4. Supprimer un événement.");
+        System.out.println("5. Retour");
+
+        try {
+            choice = Integer.parseInt(scanner.nextLine());
+
+            switch (choice) {
+                case 1 -> System.out.println("Option 1");
+                case 2 -> System.out.println("Option 2");
+                case 3 -> System.out.println("Option 3");
+                case 4 -> System.out.println("Option 4");
+                case 5 -> System.out.println("Option 5");
+                default -> System.out.println("Choix invalide.");
+            }
+        }catch (Exception e){
+            System.out.println("Erreur :" + e.getMessage());
+        }
+    }
     }
 
     public static void main(String[] args) {
