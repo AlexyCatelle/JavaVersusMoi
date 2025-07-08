e8ea41e25ec2d8eb32f1fe741d785f16ac1d8b08
AlexyCatelle <alexy.catelle@oclock.school>
2025-07-08 14:04:30 +0200

    IHM menu principal

--- src/main/java/TPBillet/IHM.java
+++ src/main/java/TPBillet/IHM.java
@@ -12,17 +12,33 @@ public class IHM {
     private Scanner scanner = new Scanner(System.in);
 
     public void start (){
-        System.out.println("Menu : \n 1 => Créer nouvel évènement. \n 2 => Voir les évènements.");
-    int choice = scanner.nextInt();
+        int choice = -1;
+        while (choice != 6) {
+            System.out.println("\n====== Menu Principal ======");
+            System.out.println("1 => Gérer les Adresses");
+            System.out.println("2 => Gérer les Lieux");
+            System.out.println("3 => Gérer les Événements");
+            System.out.println("4 => Gérer les Clients");
+            System.out.println("5 => Gérer les Tickets");
+            System.out.println("6 => Quitter");
+            System.out.print("Votre choix : ");
 
-    try {
-        switch (choice) {
-            case 1 -> System.out.println("Option 1");
-            case 2 -> System.out.println("Option 2");
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+
+                switch (choice) {
+                    case 1 -> System.out.println("Option 1");
+                    case 2 -> System.out.println("Option 2");
+                    case 3 -> System.out.println("Option 3");
+                    case 4 -> System.out.println("Option 4");
+                    case 5 -> System.out.println("Option 5");
+                    case 6 -> System.out.println("Option 6");
+                    default -> System.out.println("Choix invalide.");
+                }
+            }catch (Exception e){
+                System.out.println("Erreur :" + e.getMessage());
+            }
         }
-    }catch (Exception e){
-        System.out.println("Erreur :" + e.getMessage());
-    }
     }
 
     public static void main(String[] args) {
