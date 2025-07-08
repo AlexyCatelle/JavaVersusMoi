1bb80c3c4e5758aeb4d3011a72e413dcb2d9e916
AlexyCatelle <alexy.catelle@oclock.school>
2025-07-08 13:48:36 +0200

    IHM In progress

--- src/main/java/TPBillet/IHM.java
+++ src/main/java/TPBillet/IHM.java
@@ -11,7 +11,23 @@ public class IHM {
     private List<Event> events = new ArrayList<>();
     private Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public void start (){
+        System.out.println("Menu : \n 1 => Créer nouvel évènement. \n 2 => Voir les évènements.");
+    int choice = scanner.nextInt();
 
+    try {
+        switch (choice) {
+            case 1 -> System.out.println("Option 1");
+            case 2 -> System.out.println("Option 2");
+        }
+    }catch (Exception e){
+        System.out.println("Erreur :" + e.getMessage());
     }
+    }
+
+    public static void main(String[] args) {
+        IHM ihm = new IHM();
+        ihm.start();
+    }
+
 }
